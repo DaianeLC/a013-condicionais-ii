f7e4b3b0376cfc7577681b247bf49a4f3a087ef4
Daiane <daianelopescorrea1@gmail.com>
2022-08-09 16:45:43 -0300

    exercicio 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,17 @@
-const num = //seu numero aqui
+const num = Number(prompt('Digite um numero')) 
+//a
+if(num % 2 == 0 ){
+    if(num % 3 == 0){
+        console.log('O número escolhido é divisível por 2 ou 3')
+    }
+}else{
+    console.log('O numero escolhido não é divisível por 2 ou 3')
+}
 
+
+//b
+if(num % 2 === 0 || num % 3=== 0){
+    console.log('No segundo teste o numero escolhido é divisível por 2 ou 3')
+}else{
+    console.log('No segundo teste o numero escolhido NÃO é divisível por 2 ou 3')
+}
